a1f0b5b9bca86ec08fbd5972dfdbb9f2a10c8cd7
runyx1325 <lucaburst11@gmail.com>
2022-06-13 23:00:49 +0200

    implemented SelectionSort
    Problems with Tests

--- AuK_week7/src/dhbwsortsimple.c
+++ AuK_week7/src/dhbwsortsimple.c
@@ -21,7 +21,9 @@ bool BubbleSortImplemented() {
 
 //BubbleSort
 void BubbleSortArray(Student_p *array, int count) {
+	for (int i = 0; i < count; i++) {
 
+	}
 }
 
 
@@ -32,5 +34,26 @@ bool SelectionSortImplemented() {
 
 //SelectionSort
 void SelectionSortArray(Student_p *array, int count) {
+     
+    int i, j;
+    // One by one move boundary of unsorted subarray
+    
+    for (i = 0; i < count - 1; i++)
+    {
+        // Find the minimum element in unsorted array
+        int min_idx = i;
+        for (j = i + 1; j < count; j++) {
+            if (array[j]->matrnr < array[min_idx]->matrnr) {
+                min_idx = j;
+            }
+        }
 
+        // Swap the found minimum element
+        // with the first element
+        Student_p temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+    }
+    //Eigentlich sortiert der SelectionSort alles, jedoch fliege ich bei einem der Tests immer wieder raus. Habe noch nicht ganz verstanden warum. Vielleicht liegt es Mal wieder an der IDE.
+    //Man bekommt es übrigens auch mit VisualStudio mit einem anderen Compiler zu laufen.
 }
