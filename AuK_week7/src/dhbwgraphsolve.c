a32216df853c21f174996326799fa0cd424a2f59
Burst Luca (PEA4-Fe-DHBW-IT) <Luca.Burst@de.bosch.com>
2022-07-11 14:19:32 +0200

    graph

--- AuK_week7/src/dhbwgraphsolve.c
+++ AuK_week7/src/dhbwgraphsolve.c
@@ -10,6 +10,13 @@
 #include <stdlib.h>
 #include "dhbwgraphsolve.h"
 
+// addEdgeToNode(AdjNodeP node, AdjEdgeP edge)
+// AdjEdgeAdd(AdjNodeP *list, char first, char second, int weight)
+// otherAdjNode(AdjNodeP node, AdjEdgeP edge)
+// //-1 if edge doesn't exist
+// int AdjEdgeWeight(AdjNodeP* list, char from, char to)
+// AdjListPrintGraphviz(AdjNodeP *list, bool directed)
+// 
 //Ab hier Aufgaben
 
 //Auf true setzen, damit Prim getestet wird
@@ -19,8 +26,52 @@ bool primImplemented() {
 
 //Berechnet den minimalen Spannbaum mittels Prim's Algorithmus
 //Ausgabe: Kanten und Knoten, die zum minimalen Spannbaum gehoeren, sind markiert (marked=true)
-void primMinimalSpanningTree(AdjNodeP *list) {
+void primMinimalSpanningTree(AdjNodeP* list) {
+	
+	int countNodes = countN(list);
+	(*list)->marked = true;
+	for (int i = 0; i < countNodes - 1; i++) {
+		AdjNodeP currentNode = *list;
+		AdjEdgeP minEdge = NULL;
+		while (currentNode) {
+			AdjEdgeLP currentEdge = currentNode->edges;
+			while (currentEdge) {
+				if (possibleEdge(currentEdge->edge)) {
+					if (!minEdge) {
+						if (!currentEdge->edge->marked) {
+							minEdge = currentEdge->edge;
+						}
+					}
+					else {
+						if (!currentEdge->edge->marked) {
+							if (minEdge->weight > currentEdge->edge->weight) {
+								minEdge = currentEdge->edge;
+							}
+						}
+					}
+				}
+				currentEdge = currentEdge->next;
+			}
+			currentNode = currentNode->next;
+		}
+		minEdge->marked = true;
+		minEdge->first_node->marked = true;
+		minEdge->second_node->marked = true;
+	}
+}
 
+bool possibleEdge(AdjEdgeP edge) {
+	return !(edge->first_node->marked == true && edge->second_node->marked == true) || (edge->first_node->marked == false && edge->second_node->marked == false);
+}
+	
+
+int countN(AdjNodeP* list) {
+	int counter = 0;
+	AdjNodeP cur = *list;
+	while (cur->next != 0) {
+		counter++;
+		cur = cur->next;
+	}
 }
 
 //Auf true setzen, damit Dijkstra getestet wird
